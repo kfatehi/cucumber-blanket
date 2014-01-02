dadee563a1ea459c588b1507a4a98078e20d0dc6
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-01-01 18:39:10 -0800

    Update javascript blanket adapter to include sources data.

--- javascript/cucumber-blanket.js
+++ javascript/cucumber-blanket.js
@@ -7,7 +7,7 @@
    * Required blanket commands:
    * blanket.setupCoverage(); // Do it ASAP
    * blanket.onTestStart(); // Do it ASAP
-   * blanket.onTestDone(); // After Scenario Hook
+   * blanket.onTestDone(); // After Scenario Hook (Not necessary)
    * blanket.onTestsDone(); // After Scenario Hook
    */
 
@@ -15,6 +15,12 @@
     callback: function() {
       blanket.setupCoverage();
       blanket.onTestStart();
+      window.CUCUMBER_BLANKET = {
+        files: {},
+        sources: {},
+        done: false,
+        is_setup: true
+      };
     }
   });
 
@@ -25,6 +31,16 @@
    * that doesn't actually work so we'll override defaultReporter
    */
   blanket.defaultReporter = function(coverage_results){
-    window.COVERAGE_RESULTS = coverage_results; // We'll grab this on selenium side
+    window.CUCUMBER_BLANKET.files = coverage_results.files;
+    // Strangely enough it looks like we need to iterate in order to grab the `source`
+    // data which is necessary to know which lines of code are being reported on.
+    var files = Object.keys(coverage_results.files);
+    for (var i = 0, l = files.length; i < l; i ++) {
+      var file = files[i];
+      window.CUCUMBER_BLANKET.sources[file] = window.CUCUMBER_BLANKET.files[file].source;
+    }
+    window.CUCUMBER_BLANKET.done = true;
+    // Now we can grab all this on the selenium side through window.CUCUMBER_BLANKET
   };
 })();
+
